99b4b0d1bd5ebf5da8eee0e68e3f7565cd6f4b2a
Nick Talbot <solutions@magichat.biz>
2017-06-14 08:09:07 +0100

    [NT]: Alfresco document store upload implementation added

--- app/controllers/SampleReportController.java
+++ app/controllers/SampleReportController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import controllers.base.WizardController;
 import data.SampleReportData;
+import interfaces.DocumentStore;
 import interfaces.PdfGenerator;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -9,6 +10,7 @@ import java.util.concurrent.CompletionStage;
 import javax.inject.Inject;
 import org.apache.commons.lang3.ArrayUtils;
 import play.Environment;
+import play.Logger;
 import play.data.FormFactory;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Result;
@@ -16,13 +18,19 @@ import play.mvc.Result;
 public class SampleReportController extends WizardController<SampleReportData>
 {
     private final PdfGenerator pdfGenerator;
+    private final DocumentStore documentStore;
 
     @Inject
-    public SampleReportController(HttpExecutionContext ec, Environment environment, FormFactory formFactory, PdfGenerator pdfGenerator) {
+    public SampleReportController(HttpExecutionContext ec,
+                                  Environment environment,
+                                  FormFactory formFactory,
+                                  PdfGenerator pdfGenerator,
+                                  DocumentStore documentStore) {
 
         super(ec, environment, formFactory, SampleReportData.class, "views.html.sampleReport.page");
 
         this.pdfGenerator = pdfGenerator;
+        this.documentStore = documentStore;
     }
 
     @Override
@@ -30,7 +38,12 @@ public class SampleReportController extends WizardController<SampleReportData>
 
         sampleReportData.setReportDate(new SimpleDateFormat("dd MMMM yyy").format(new Date()));
 
-        return pdfGenerator.generate("helloWorld", sampleReportData).thenApply(result ->
-                ok(ArrayUtils.toPrimitive(result)).as("application/pdf"));
+        Logger.info("Sample Report Data: " + sampleReportData);
+
+        return pdfGenerator.generate("helloWorld", sampleReportData).thenApply(result -> {
+
+//              documentStore.uploadNewPdf(result, "sampleReport.pdf", "someUser", "crn", "author", 12345);
+                return ok(ArrayUtils.toPrimitive(result)).as("application/pdf");
+        });
     }
 }
