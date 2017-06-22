17e3c5e2232413c0f8b58dc8a8fcf319ed7e7985
Paul Massey <paul.massey@scriptwerx.io>
2017-06-22 15:19:19 +0100

    [PM]: Included page navigation.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,17 @@
 $(function() {
     var showHideContent = new GOVUK.ShowHideContent();
     showHideContent.init();
+
+  $('.nav-item').click(function(e) {
+
+    e.preventDefault();
+
+    var target = $(this).data('target');
+
+    console.log('Jumping to page:', target);
+
+    $('#jumpNumber').val(target);
+    $('#reportForm').submit();
+  });
+
 });
