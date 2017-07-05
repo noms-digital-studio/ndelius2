700bd6601629644b0c0aaa2dfb2547a9bbd75ac8
Paul Massey <paul.massey@scriptwerx.io>
2017-07-05 16:09:05 +0100

    [PM]: Included novalidate on forms.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,111 +1,74 @@
-$(function () {
+(function($) {
 
-  // Show/hide content
-  var showHideContent = new GOVUK.ShowHideContent();
-  showHideContent.init();
+  'use strict';
 
-  /**
-   * Date picker
-   */
-  $('.date-picker').datepicker({
-    dateFormat: 'dd MM yy'
-  }).parent().addClass('date-wrapper');
+  $(function () {
 
-  /**
-   * Method to show/hide under/over recommended character limit elements/messages.
-   * @param {Boolean} over Text entry over the recommended limit
-   * @param {HTMLElement} targetUnder Under limit element
-   * @param {HTMLElement} targetOver Over limit element
-   */
-  function toggleCountMessage(over, targetUnder, targetOver) {
-    if (over) {
-      targetUnder.addClass('js-hidden');
-      targetOver.removeClass('js-hidden');
-    } else {
-      targetUnder.removeClass('js-hidden');
-      targetOver.addClass('js-hidden');
+    // Show/hide content
+    var showHideContent = new GOVUK.ShowHideContent();
+    showHideContent.init();
+
+    // Stick at top when scrolling
+    GOVUK.stickAtTopWhenScrolling.init();
+
+    // Autosize all Textarea elements
+    autosize(document.querySelectorAll('textarea'));
+
+    /**
+     * Method to show/hide under/over recommended character limit elements/messages.
+     * @param {Boolean} over Text entry over the recommended limit
+     * @param {HTMLElement} targetUnder Under limit element
+     * @param {HTMLElement} targetOver Over limit element
+     */
+    function toggleCountMessage(over, targetUnder, targetOver) {
+      if (over) {
+        targetUnder.addClass('js-hidden');
+        targetOver.removeClass('js-hidden');
+      } else {
+        targetUnder.removeClass('js-hidden');
+        targetOver.addClass('js-hidden');
+      }
     }
-  }
 
-  /**
-   * Textarea elements
-   */
-  $('textarea').keyup(function() {
-    var textArea = $(this),
-      limit = textArea.data('limit'),
-      current = textArea.val().length,
-      targetUnder,
-      targetOver;
+    /**
+     * Textarea elements
+     */
+    $('textarea').keyup(function() {
+      var textArea = $(this),
+        limit = textArea.data('limit'),
+        current = textArea.val().length,
+        targetUnder,
+        targetOver;
 
-    if (limit && textArea.data('target')) {
-      targetUnder = $('#' + textArea.data('target') + '-under');
-      targetOver = $('#' + textArea.data('target') + '-over');
+      if (limit && textArea.data('target')) {
+        targetUnder = $('#' + textArea.data('target') + '-under');
+        targetOver = $('#' + textArea.data('target') + '-over');
 
-      toggleCountMessage(current > limit, targetUnder, targetOver);
-    }
+        toggleCountMessage(current > limit, targetUnder, targetOver);
+      }
+    });
+
+    /**
+     * Navigation items
+     */
+    $('.nav-item').click(function(e) {
+
+      e.preventDefault();
+
+      var target = $(this).data('target');
+      if (!$(this).hasClass('active')) {
+        $('#jumpNumber').val(target);
+        $('form').submit();
+      }
+    });
+
+    /**
+     * Date picker
+     */
+    $('.date-picker').datepicker({
+      dateFormat: 'dd MM yy'
+    }).parent().addClass('date-wrapper');
+
   });
 
-  /**
-   * Autosize all Textarea elements
-   */
-  autosize(document.querySelectorAll('textarea'));
-
-  /**
-   * Method to show/hide under/over recommended character limit elements/messages.
-   * @param {Boolean} over Text entry over the recommended limit
-   * @param {HTMLElement} targetUnder Under limit element
-   * @param {HTMLElement} targetOver Over limit element
-   */
-  function toggleCountMessage(over, targetUnder, targetOver) {
-    if (over) {
-      targetUnder.addClass('js-hidden');
-      targetOver.removeClass('js-hidden');
-    } else {
-      targetUnder.removeClass('js-hidden');
-      targetOver.addClass('js-hidden');
-    }
-  }
-
-  /**
-   * Textarea elements
-   */
-  $('textarea').keyup(function() {
-    var textArea = $(this),
-      limit = textArea.data('limit'),
-      current = textArea.val().length,
-      targetUnder,
-      targetOver;
-
-    if (limit && textArea.data('target')) {
-      targetUnder = $('#' + textArea.data('target') + '-under');
-      targetOver = $('#' + textArea.data('target') + '-over');
-
-      toggleCountMessage(current > limit, targetUnder, targetOver);
-    }
-  });
-
-  /**
-   * Autosize all Textarea elements
-   */
-  autosize(document.querySelectorAll('textarea'));
-
-  /**
-   *
-   */
-  GOVUK.stickAtTopWhenScrolling.init();
-
-  /**
-   * Navigation items
-   */
-  $('.nav-item').click(function(e) {
-
-    e.preventDefault();
-
-    var target = $(this).data('target');
-    if (!$(this).hasClass('active')) {
-      $('#jumpNumber').val(target);
-      $('form').submit();
-    }
-  });
-
-});
+})(window.jQuery);
