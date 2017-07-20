c89ba5d4cdcb40bf5aa69944c2242d01ee5f2ed9
Paul Massey <paul.massey@scriptwerx.io>
2017-07-20 11:35:03 +0100

    [PM]: Updated IE8 support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,74 +1,77 @@
-(function($) {
+(function ($) {
 
-  'use strict';
+    'use strict';
 
-  $(function () {
+    $(function () {
 
-    // Show/hide content
-    var showHideContent = new GOVUK.ShowHideContent();
-    showHideContent.init();
+        // Show/hide content
+        var showHideContent = new GOVUK.ShowHideContent();
 
-    // Stick at top when scrolling
-    GOVUK.stickAtTopWhenScrolling.init();
+        showHideContent.init();
 
-    // Autosize all Textarea elements
-    autosize(document.querySelectorAll('textarea'));
+        // Stick at top when scrolling
+        GOVUK.stickAtTopWhenScrolling.init();
 
-    /**
-     * Method to show/hide under/over recommended character limit elements/messages.
-     * @param {Boolean} over Text entry over the recommended limit
-     * @param {HTMLElement} targetUnder Under limit element
-     * @param {HTMLElement} targetOver Over limit element
-     */
-    function toggleCountMessage(over, targetUnder, targetOver) {
-      if (over) {
-        targetUnder.addClass('js-hidden');
-        targetOver.removeClass('js-hidden');
-      } else {
-        targetUnder.removeClass('js-hidden');
-        targetOver.addClass('js-hidden');
-      }
-    }
+        // Autosize all Textarea elements (does not support IE8).
+        if (!$('html').is('.lte-ie8')) {
+            autosize(document.querySelectorAll('textarea'));
+        }
 
-    /**
-     * Textarea elements
-     */
-    $('textarea').keyup(function() {
-      var textArea = $(this),
-        limit = textArea.data('limit'),
-        current = textArea.val().length,
-        targetUnder,
-        targetOver;
+        /**
+         * Method to show/hide under/over recommended character limit elements/messages.
+         * @param {Boolean} over Text entry over the recommended limit
+         * @param {HTMLElement} targetUnder Under limit element
+         * @param {HTMLElement} targetOver Over limit element
+         */
+        function toggleCountMessage(over, targetUnder, targetOver) {
+            if (over) {
+                targetUnder.addClass('js-hidden');
+                targetOver.removeClass('js-hidden');
+            } else {
+                targetUnder.removeClass('js-hidden');
+                targetOver.addClass('js-hidden');
+            }
+        }
 
-      if (limit && textArea.data('target')) {
-        targetUnder = $('#' + textArea.data('target') + '-under');
-        targetOver = $('#' + textArea.data('target') + '-over');
+        /**
+         * Textarea elements
+         */
+        $('textarea').keyup(function () {
+            var textArea = $(this),
+                limit = textArea.data('limit'),
+                current = textArea.val().length,
+                targetUnder,
+                targetOver;
 
-        toggleCountMessage(current > limit, targetUnder, targetOver);
-      }
+            if (limit && textArea.data('target')) {
+                targetUnder = $('#' + textArea.data('target') + '-under');
+                targetOver = $('#' + textArea.data('target') + '-over');
+
+                toggleCountMessage(current > limit, targetUnder, targetOver);
+            }
+        });
+
+        /**
+         * Navigation items
+         */
+        $('.nav-item').click(function (e) {
+
+            e.preventDefault();
+
+            var target = $(this).data('target');
+            if (!$(this).hasClass('active')) {
+                $('#jumpNumber').val(target);
+                $('form').submit();
+            }
+        });
+
+        /**
+         * Date picker
+         */
+        $('.date-picker').datepicker({
+            dateFormat: 'dd MM yy'
+        }).parent().addClass('date-wrapper');
+
     });
-
-    /**
-     * Navigation items
-     */
-    $('.nav-item').click(function(e) {
-
-      e.preventDefault();
-
-      var target = $(this).data('target');
-      if (!$(this).hasClass('active')) {
-        $('#jumpNumber').val(target);
-        $('form').submit();
-      }
-    });
-
-    /**
-     * Date picker
-     */
-    $('.date-picker').datepicker({
-      dateFormat: 'dd MM yy'
-    }).parent().addClass('date-wrapper');
-
-  });
 
 })(window.jQuery);
