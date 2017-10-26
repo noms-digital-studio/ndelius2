762a1fde0f438b831561a1b293e1276e453c22fa
Nick Talbot <solutions@magichat.biz>
2017-10-26 22:09:16 +0100

    [NT]: Added test to confirm JSON serialisation

---
+++ test/controllers/JsonTest.java
@@ -0,0 +1,64 @@
+package controllers;
+
+import data.ShortFormatPreSentenceReportData;
+import helper.TestableWizardController;
+import interfaces.DocumentStore;
+import interfaces.PdfGenerator;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import lombok.val;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JsonTest implements PdfGenerator, DocumentStore {
+
+    private String updatedData;
+
+    @Test
+    public void testJsonSerialisationOfEscapedCharacters() {
+
+        val controller = new TestableWizardController(this, this);
+
+        val data = new ShortFormatPreSentenceReportData();
+
+        data.setPageNumber(1);
+        data.setDocumentId("12345");
+        data.setName("This has \"quotes\" and 'apostrophes' and \\ backslashes");
+
+        controller.testStoreReport(data);
+
+        assertTrue(updatedData.contains("This has \\\"quotes\\\" and 'apostrophes' and \\\\ backslashes"));
+    }
+
+    @Override
+    public <T> CompletionStage<Byte[]> generate(String templateName, T values) {
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletionStage<Map<String, String>> updateExistingPdf(Byte[] document, String filename, String onBehalfOfUser, String updatedData, String documentId) {
+
+        this.updatedData = updatedData;
+        return null;
+    }
+
+    @Override
+    public CompletionStage<Map<String, String>> uploadNewPdf(Byte[] document, String filename, String onBehalfOfUser, String originalData, String crn, Long entityId) {
+        return null;
+    }
+
+    @Override
+    public CompletionStage<String> retrieveOriginalData(String documentId, String onBehalfOfUser) {
+        return null;
+    }
+
+    @Override
+    public CompletionStage<Integer> lockDocument(String onBehalfOfUser, String documentId) {
+        return null;
+    }
+}
+
+//
---
+++ test/helper/TestableWizardController.java
@@ -0,0 +1,30 @@
+package helper;
+
+import com.typesafe.config.ConfigFactory;
+import controllers.ShortFormatPreSentenceReportController;
+import controllers.base.EncryptedFormFactory;
+import data.ShortFormatPreSentenceReportData;
+import interfaces.DocumentStore;
+import interfaces.PdfGenerator;
+
+public class TestableWizardController extends ShortFormatPreSentenceReportController {
+
+    public TestableWizardController(PdfGenerator pdfGenerator, DocumentStore documentStore) {
+
+        super(
+                null,
+                null,
+                ConfigFactory.load(),
+                null,
+                null,
+                new EncryptedFormFactory(null, null, null),
+                pdfGenerator,
+                documentStore
+        );
+    }
+
+    public void testStoreReport(ShortFormatPreSentenceReportData data) {
+
+        nextPage(data);
+    }
+}
