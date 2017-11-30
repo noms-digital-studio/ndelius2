bd2597e0a666593f47cba5c250fc5c97d00ac8f9
Andy Marke <andy.marke@gmail.com>
2017-11-30 15:30:57 +0000

    [AM] load justiceAreas.txt with explicit CharSet to avoid errors where default is not UTF8

--- app/injection/AmazonDynamoDBProvider.java
+++ app/injection/AmazonDynamoDBProvider.java
@@ -10,7 +10,12 @@ public class AmazonDynamoDBProvider implements Provider<AmazonDynamoDB> {
     @Override
     public AmazonDynamoDB get() {
 
+        // this requires a default region to run e.g AWS_REGION environment variable set or aws config containing a default region
+        // for instance in Circle CI this is set to a random AWS region just so factory function does not fail
         return AmazonDynamoDBClientBuilder.defaultClient();
+
+
+        // use configuration below  when running locally against DynamoDB - NB region is not important
 //        return AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
 //                new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2")
 //        ).build();
