b6945a2b2cbac73046df66aeeb78aefc6abb4446
Andy Marke <andy.marke@gmail.com>
2018-01-15 13:44:33 +0000

    [AM] Added paging to interface

--- test/controllers/NationalSearchControllerTest.java
+++ test/controllers/NationalSearchControllerTest.java
@@ -15,6 +15,7 @@ import play.test.WithApplication;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
 import static play.inject.Bindings.bind;
 import static play.mvc.Http.Status.OK;
@@ -37,12 +38,12 @@ public class NationalSearchControllerTest extends WithApplication {
 
     @Test
     public void searchTermReturnsResults() {
-        when(elasticSearch.search(any())).thenReturn(completedFuture(new OffenderSearchResult()));
+        when(elasticSearch.search(any(), anyInt(), anyInt())).thenReturn(completedFuture(new OffenderSearchResult()));
         val request = new Http.RequestBuilder().method(GET).uri("/searchOffender/smith");
         val result = route(app, request);
 
         assertEquals(OK, result.status());
-        assertEquals("{\"offenders\":[],\"suggestions\":[]}", contentAsString(result));
+        assertEquals("{\"offenders\":[],\"suggestions\":[],\"total\":0}", contentAsString(result));
     }
 
     @Override
