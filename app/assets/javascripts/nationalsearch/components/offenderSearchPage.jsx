0e88ead3af58cbeea0b1011e85e8cb0fdce6b968
Andy Marke <andy.marke@gmail.com>
2018-01-17 17:41:05 +0000

    [AM] Added firstName suggestion

--- app/assets/javascripts/nationalsearch/components/offenderSearchPage.jsx
+++ app/assets/javascripts/nationalsearch/components/offenderSearchPage.jsx
@@ -2,6 +2,7 @@ import OffenderSearchResults from '../containers/offenderSearchResultsContainer'
 import OffenderSearch from '../containers/offenderSearchContainer';
 import FrameNavigation from '../containers/frameNavigationContainer';
 import AddNewOffenderLink from '../containers/addNewOffenderLinkContainer';
+import Suggestions from '../containers/suggestionsContainer';
 import GovUkPhaseBanner from './govukPhaseBanner.jsx';
 
 export default () => (
@@ -14,6 +15,7 @@ export default () => (
                     <div className="govuk-box-highlight blue">
                         <h1 className="heading-large no-margin-top margin-bottom medium">Search for an offender</h1>
                         <form className="padding-left-right"><OffenderSearch/></form>
+                        <Suggestions/>
                         <p className="bold margin-top medium no-margin-bottom">Can't find who you are looking for? <AddNewOffenderLink/></p></div>
                 </div>
                 <div className="padded mobile-pad">
