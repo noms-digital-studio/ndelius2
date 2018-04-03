e7ab452e94686694406e8a99643870d36dc88e86
Andy Marke <andy.marke@digital.justice.gov.uk>
2018-04-03 14:59:14 +0100

    [AM] 294 Phase banner now BETA (#100)

--- app/assets/javascripts/nationalsearch/components/govukPhaseBanner.js
+++ app/assets/javascripts/nationalsearch/components/govukPhaseBanner.js
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types'
 const GovUkPhaseBanner = ({basicVersion}) => {
     if (basicVersion) {
         return (
-            <div className="phase-banner mobile-pad no-cell key-content"><p><strong className="phase-tag">ALPHA</strong><span>This is a new service – your feedback will help us to improve it.&nbsp;</span><span/></p></div>
+            <div className="phase-banner mobile-pad no-cell key-content"><p><strong className="phase-tag">BETA</strong><span>This is a new service – your feedback will help us to improve it.&nbsp;</span><span/></p></div>
         )
     }
     return (
-        <div className="phase-banner phase-banner-boxed mobile-pad no-cell key-content"><p><strong className="phase-tag">ALPHA</strong><span>This is a new service – your <Link tabIndex="1" to='feedback'>feedback</Link> will help us to improve it.&nbsp;</span>Access the <LegacySearchLink tabIndex="1">previous search</LegacySearchLink> here.<span/></p></div>
+        <div className="phase-banner phase-banner-boxed mobile-pad no-cell key-content"><p><strong className="phase-tag">BETA</strong><span>This is a new service – your <Link tabIndex="1" to='feedback'>feedback</Link> will help us to improve it.&nbsp;</span>Access the <LegacySearchLink tabIndex="1">previous search</LegacySearchLink> here.<span/></p></div>
     )
 };
 
