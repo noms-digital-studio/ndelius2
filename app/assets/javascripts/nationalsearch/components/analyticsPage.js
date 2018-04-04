60c4ba35249256a9fb9ea10eb1fff26931265226
Andy Marke <andy.marke@digital.justice.gov.uk>
2018-04-04 15:41:59 +0100

    297 analytics refresh (#104)
    * [AM] 297 Auto refresh analytics

--- app/assets/javascripts/nationalsearch/components/analyticsPage.js
+++ app/assets/javascripts/nationalsearch/components/analyticsPage.js
@@ -19,6 +19,8 @@ class AnalyticsPage extends Component {
     }
     componentWillMount() {
         fetch(this.props)
+        const interval = setInterval(() => fetch(this.props), 30000)
+        if (interval.unref) {interval.unref()} // when running in mocha/node unref so test doesn't hang
     }
     componentWillReceiveProps(nextProps) {
         fetch(nextProps)
@@ -51,14 +53,12 @@ class AnalyticsPage extends Component {
                 </div>
             </div>)
     }
-
 }
 
 const fetch = props => {
     const {fetchVisitCounts, currentTimeRange} = props;
     fetchVisitCounts(currentTimeRange)
 }
-
 
 const NavigationPanel = () => (
             <nav className="js-stick-at-top-when-scrolling">
