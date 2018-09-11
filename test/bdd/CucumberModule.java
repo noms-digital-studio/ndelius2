7b8ea58748681e8f1f8d539cd51a96bf8e933758
Andy Marke <andy.marke@gmail.com>
2018-09-11 10:30:01 +0100

    88 Parole Recommendation (add jetty timeout)

--- test/bdd/CucumberModule.java
+++ test/bdd/CucumberModule.java
@@ -12,9 +12,9 @@ public class CucumberModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(TestBrowser.class).toInstance(ChromeTestBrowser.create(play.api.test.Helpers.testServerPort()));
-        bind(WireMockServer.class).annotatedWith(Names.named("pdfWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.PDF.getPort())));
-        bind(WireMockServer.class).annotatedWith(Names.named("alfrescofWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.ALFRESCO.getPort())));
-        bind(WireMockServer.class).annotatedWith(Names.named("offenderApiWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.OFFENDER_API.getPort())));
+        bind(WireMockServer.class).annotatedWith(Names.named("pdfWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.PDF.getPort()).jettyStopTimeout(10000L)));
+        bind(WireMockServer.class).annotatedWith(Names.named("alfrescofWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.ALFRESCO.getPort()).jettyStopTimeout(10000L)));
+        bind(WireMockServer.class).annotatedWith(Names.named("offenderApiWireMock")).toInstance( new WireMockServer(wireMockConfig().port(Ports.OFFENDER_API.getPort()).jettyStopTimeout(10000L)));
     }
 
 }
