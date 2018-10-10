1919a68a029265eada0ec2e6f9e0412977ecb7f6
Andy Marke <andy.marke@gmail.com>
2018-10-10 11:53:49 +0100

    181 Attempt to disable browser back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -442,4 +442,33 @@ function openPopup(url, name, top, left) {
             $('#roleother-section').addClass('js-hidden');
         }
     });
+
+    (function (global) {
+
+        if(typeof (global) === "undefined")
+        {
+            throw new Error("window is undefined");
+        }
+
+        var _hash = "!";
+        var noBackPlease = function () {
+            global.location.href += "#";
+
+            global.setTimeout(function () {
+                global.location.href += "!";
+            }, 50);
+        };
+
+        global.onhashchange = function () {
+            if (global.location.hash !== _hash) {
+                global.location.hash = _hash;
+            }
+        };
+
+        global.onload = function () {
+            noBackPlease();
+        };
+
+    })(window);
+
 })(window.jQuery);
