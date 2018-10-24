e0c081551081da858c5aac9e2a6909e9c63857a5
Paul Massey <theDustRoom@users.noreply.github.com>
2018-10-24 10:09:02 +0100

    Changed to 1024 x 768 (#237)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,7 @@ function formWithZeroJumpNumber(form) {
 }
 
 function openPopup(url, name, top, left) {
-    window.open(url, (name || 'reportpopup'), 'top=' + (top || '200') + ',left=' + (left || '0') + ',height=760,width=820,resizable=yes,scrollbars=yes,location=no,menubar=no,status=yes,toolbar=no').focus();
+    window.open(url, (name || 'reportpopup'), 'top=' + (top || '200') + ',left=' + (left || '0') + ',width=1024,height=768,resizable=yes,scrollbars=yes,location=no,menubar=no,status=yes,toolbar=no').focus();
 }
 
 (function ($) {
