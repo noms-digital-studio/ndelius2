1fe3df2fc56a7f6a05eb522f4d7af9ab97e6bfd9
Andy Marke <andy.marke@digital.justice.gov.uk>
2018-11-21 11:43:20 +0000

    271 Restored National Search GA outcomes (#262)

--- app/assets/javascripts/nationalsearch/actions/navigate.js
+++ app/assets/javascripts/nationalsearch/actions/navigate.js
@@ -3,26 +3,42 @@ export const LEGACY_SEARCH = 'LEGACY_SEARCH'
 export const SHOW_OFFENDER_DETAILS = 'SHOW_OFFENDER_DETAILS'
 export const ADD_NEW_OFFENDER = 'ADD_NEW_OFFENDER'
 
-export const showOffenderDetails = (offenderId) => (
+const recordSearchOutcome = (data) => {
+
+    if (typeof gtag === 'function') {
+        gtag('event', data.type, {
+            'event_category': 'search',
+            'event_label': 'Search Outcome: ' + (data.rankIndex ? data.type + (' (Rank: ' + data.rankIndex + ')') : ''),
+            'value': data.rankIndex || 0
+        })
+
+        virtualPageLoad(data.type.replace('search-', ''))
+    }
+}
+export const showOffenderDetails = (offenderId, rankIndex = {}) => (
     dispatch => {
+        recordSearchOutcome({ type: 'search-offender-details', rankIndex })
         dispatch({type: SHOW_OFFENDER_DETAILS, offenderId})
     }
 )
 
-export const addContact = (offenderId) => (
+export const addContact = (offenderId, rankIndex = {}) => (
     dispatch => {
+        recordSearchOutcome({ type: 'search-add-contact', rankIndex })
         dispatch({type: ADD_CONTACT, offenderId})
     }
 )
 
 export const legacySearch = () => (
     dispatch => {
+        recordSearchOutcome({ type: 'search-legacy-search'})
         dispatch({type: LEGACY_SEARCH})
     }
 )
 
 export const addNewOffender = () => (
     dispatch => {
+        recordSearchOutcome({ type: 'search-add-new-offender'})
         dispatch({type: ADD_NEW_OFFENDER})
     }
 )
--- test/assets/javascripts/nationalsearch/actions/navigateTest.js
+++ test/assets/javascripts/nationalsearch/actions/navigateTest.js
@@ -11,14 +11,26 @@ describe('navigate action', () => {
         global.$ = {
             ajax: stub()
         }
+        global.gtag = stub()
+        global.virtualPageLoad = stub()
     })
 
     describe('on addContact', () => {
         beforeEach(() => {
-            addContact(1234567)(dispatch)
+            addContact(1234567, 2, {firstName: ["Josephina"], surname: ["Se"]})(dispatch)
         })
         it ('dispatches ADD_CONTACT', () => {
             expect(dispatch).to.be.calledWith({type: 'ADD_CONTACT', offenderId: 1234567})
+        })
+        it ('calls google analytics endpoint with event and rank', () => {
+            expect(global.gtag).to.be.calledWith('event', 'search-add-contact', {
+                'event_category': 'search',
+                'event_label': 'Search Outcome: search-add-contact (Rank: 2)',
+                'value': 2
+            })
+        })
+        it ('calls google analytics virtual load', () => {
+            expect(global.virtualPageLoad).to.be.calledWith('add-contact')
         })
     })
     describe('on addNewOffender', () => {
@@ -28,13 +40,33 @@ describe('navigate action', () => {
         it ('dispatches ADD_NEW_OFFENDER', () => {
             expect(dispatch).to.be.calledWith({type: 'ADD_NEW_OFFENDER'})
         })
+        it ('calls google analytics endpoint with event', () => {
+            expect(global.gtag).to.be.calledWith('event', 'search-add-new-offender', {
+                'event_category': 'search',
+                'event_label': 'Search Outcome: ',
+                'value': 0
+            })
+        })
+        it ('calls google analytics virtual load', () => {
+            expect(global.virtualPageLoad).to.be.calledWith('add-new-offender')
+        })
     })
     describe('on showOffenderDetails', () => {
         beforeEach(() => {
-            showOffenderDetails(1234567)(dispatch)
+            showOffenderDetails(1234567, 2, {firstName: ["Josephina"], surname: ["Se"]})(dispatch)
         })
         it ('dispatches SHOW_OFFENDER_DETAILS', () => {
             expect(dispatch).to.be.calledWith({type: 'SHOW_OFFENDER_DETAILS', offenderId: 1234567})
+        })
+        it ('calls google analytics endpoint with event and rank', () => {
+            expect(global.gtag).to.be.calledWith('event', 'search-offender-details', {
+                'event_category': 'search',
+                'event_label': 'Search Outcome: search-offender-details (Rank: 2)',
+                'value': 2
+            })
+        })
+        it ('calls google analytics virtual load', () => {
+            expect(global.virtualPageLoad).to.be.calledWith('offender-details')
         })
     })
     describe('on legacySearch', () => {
@@ -43,6 +75,16 @@ describe('navigate action', () => {
         })
         it ('dispatches LEGACY_SEARCH', () => {
             expect(dispatch).to.be.calledWith({type: 'LEGACY_SEARCH'})
+        })
+        it ('calls google analytics endpoint with event', () => {
+            expect(global.gtag).to.be.calledWith('event', 'search-legacy-search', {
+                'event_category': 'search',
+                'event_label': 'Search Outcome: ',
+                'value': 0
+            })
+        })
+        it ('calls google analytics virtual load', () => {
+            expect(global.virtualPageLoad).to.be.calledWith('legacy-search')
         })
     })
 
