d5f5e4e3abcd3dc040a4917261e8a116e954a6fe
Andy Marke <andy.marke@gmail.com>
2018-12-06 14:16:03 +0000

    corrected fetch path to work in newTech context

--- app/assets/javascripts/offendersummary/api/offender.js
+++ app/assets/javascripts/offendersummary/api/offender.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch'
 
 export default {
-    getDetails: (cb, rejected) => fetch('../offender/detail', {cache: "no-store"})
+    getDetails: (cb, rejected) => fetch('offender/detail', {cache: "no-store"})
         .then(response => response.json())
         .then(data => cb(data))
         .catch(error => rejected(error))
