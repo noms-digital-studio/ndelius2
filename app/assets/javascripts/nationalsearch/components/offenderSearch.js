878ce9808fd1fa5b33bcc838ffb158d521598a82
Paul Massey <paul.massey@scriptwerx.io>
2018-12-12 14:41:50 +0000

    Dragon - Try search value

--- app/assets/javascripts/nationalsearch/components/offenderSearch.js
+++ app/assets/javascripts/nationalsearch/components/offenderSearch.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const OffenderSearch = ({searchTerm, probationAreasFilter, search, searchType}) => {
+const OffenderSearch = ({ searchTerm, probationAreasFilter, search, searchType }) => {
 
     const onSubmit = (event) => {
-        search(searchTerm, searchType, probationAreasFilter)
-        event.preventDefault()
-    }
+        event.preventDefault();
+        let inputValue = document.getElementById('searchTerms').value;
+        search(searchTerm || inputValue, searchType, probationAreasFilter);
+    };
 
     return (
-        <form className="padding-left-right" onSubmit={(event) => onSubmit(event)}>
+        <form className="padding-left-right" onSubmit={ (event) => onSubmit(event) }>
             <label htmlFor="searchTerms" className='visually-hidden'>Results will be updated as you type</label>
             <input id="searchTerms" name="searchTerms" type="search" tabIndex="1" role='searchbox'
-                   autoFocus={true} className="form-control national-search"
+                   autoFocus={ true } className="form-control national-search"
                    placeholder="Any combination of names, dates of birth, ID numbers, towns and postcodes"
-                   value={searchTerm}
-                   onChange={event => search(event.target.value, searchType, probationAreasFilter)} />
+                   value={ searchTerm }
+                   onChange={ event => search(event.target.value, searchType, probationAreasFilter) }/>
         </form>
-    )
+    );
 };
 
 OffenderSearch.propTypes = {
     searchTerm: PropTypes.string.isRequired,
     search: PropTypes.func.isRequired,
     probationAreasFilter: PropTypes.arrayOf(PropTypes.string).isRequired,
-    searchType: PropTypes.string.isRequired,
+    searchType: PropTypes.string.isRequired
 };
 
 export default OffenderSearch;
