f31000df8cae81692d47796ad6441720ab48ae20
Andy Marke <andy.marke@gmail.com>
2019-01-18 10:20:28 +0000

    fix bdd

--- test/bdd/RunCucumber.java
+++ test/bdd/RunCucumber.java
@@ -6,6 +6,6 @@ import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty"})
+@CucumberOptions(features={"features/bdd"}, format = {"pretty"})
 public class RunCucumber {
 }
