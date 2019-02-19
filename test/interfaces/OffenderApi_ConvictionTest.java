cde35ec373515a2695433cac4f2f5edf7d7ce084
Andy Marke <andy.marke@gmail.com>
2019-02-19 16:45:44 +0000

    434 Added offence count to parole offences

--- test/interfaces/OffenderApi_ConvictionTest.java
+++ test/interfaces/OffenderApi_ConvictionTest.java
@@ -48,6 +48,56 @@ public class OffenderApi_ConvictionTest {
     }
 
     @Test
+    public void itAppendsOffenceCountWhenPresentAndGreaterThanOne() {
+        val mainOffence = OffenderApi.Offence.builder()
+            .mainOffence(true)
+            .offenceDate("2016-12-24T00:00")
+            .offenceCount(3L)
+            .detail(OffenceDetail.builder()
+                .code("not used")
+                .subCategoryDescription("Main")
+                .build())
+            .build();
+
+        val additionalOffence1 = OffenderApi.Offence.builder()
+            .mainOffence(false)
+            .offenceDate("2017-07-01T00:00")
+            .offenceCount(1L)
+            .detail(OffenceDetail.builder()
+                .code("not used")
+                .subCategoryDescription("Additional one")
+                .build())
+            .build();
+
+        val additionalOffence2 = OffenderApi.Offence.builder()
+            .mainOffence(false)
+            .offenceDate("2018-08-01T00:00")
+            .offenceCount(2L)
+            .detail(OffenceDetail.builder()
+                .code("not used")
+                .subCategoryDescription("Additional two")
+                .build())
+            .build();
+
+        val additionalOffence3 = OffenderApi.Offence.builder()
+            .mainOffence(false)
+            .offenceDate("2017-06-01T00:00")
+            .detail(OffenceDetail.builder()
+                .code("not used")
+                .subCategoryDescription("Additional three")
+                .build())
+            .build();
+
+        val offences = ImmutableList.of(mainOffence, additionalOffence1, additionalOffence2, additionalOffence3);
+
+        val conviction = OffenderApi.Conviction.builder()
+            .offences(offences)
+            .build();
+
+        assertThat(conviction.allOffenceDescriptions()).isEqualTo("Main x 3 - 24/12/2016<br>Additional two x 2 - 01/08/2018<br>Additional one - 01/07/2017<br>Additional three - 01/06/2017");
+    }
+
+    @Test
     public void itHandlesAdditionalOffencesWithNoDateCorrectly() {
         val mainOffence = OffenderApi.Offence.builder()
             .mainOffence(true)
