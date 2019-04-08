7d9826cb1bbddbca277b82ff651fbfb3b40d9527
Paul Massey <paul.massey@scriptwerx.io>
2019-04-08 15:45:58 +0100

    Minor refactor

--- app/assets/javascripts/app/helpers/popupHelper.js
+++ app/assets/javascripts/app/helpers/popupHelper.js
@@ -1,5 +1,5 @@
 const openPopupWindow = window.openPopup = (url, name, top, left) => {
-  window.open(url, (name || 'reportpopup'), 'top=' + (top || '200') + ',left=' + (left || '0') + ',width=1024,height=768,resizable=yes,scrollbars=yes,location=no,menubar=no,status=yes,toolbar=no').focus()
+  window.open(url, (name || 'reportpopup'), `top=${ top || '200' },left=${ left || '0' },width=1024,height=768,resizable=yes,scrollbars=yes,location=no,menubar=no,status=yes,toolbar=no`).focus()
 }
 
 export {
