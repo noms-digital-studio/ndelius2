e015cfd88d0a06bb2b4e83953d60bef7fd790d96
Paul Massey <paul.massey@scriptwerx.io>
2019-05-13 09:11:28 +0100

    Reinstated skin URL setting for TinyMCE

--- app/assets/javascripts/app/components/textAreas.js
+++ app/assets/javascripts/app/components/textAreas.js
@@ -162,6 +162,7 @@ const initTextAreas = () => {
     paste_enable_default_filters: 'false',
     paste_word_valid_elements: 'p,b,i,strong,em,ol,ul,li',
     document_base_url: `${ localPath.substr(0, localPath.indexOf('/') + 1) }`,
+    skin_url: `${ localPath }skins/ui/oxide`,
     setup: $editor => {
       $editor.on('init', () => {
         configureEditor($editor)
