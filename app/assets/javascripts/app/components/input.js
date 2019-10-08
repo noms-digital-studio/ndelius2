acbab3227814c58ad49eb479f56e9a1c16d1ed21
Paul Massey <paul.massey@scriptwerx.io>
2019-10-08 15:58:54 +0100

    Remove debounce from input

--- app/assets/javascripts/app/components/input.js
+++ app/assets/javascripts/app/components/input.js
@@ -1,14 +1,13 @@
 import { nodeListForEach } from '../utilities/nodeListForEach'
-import { debounce } from '../utilities/debounce'
 import { autoSaveProgress } from '../helpers/saveProgressHelper'
 
 const initInputs = () => {
 
   nodeListForEach(document.querySelectorAll('input:not([type="hidden"])'), $input => {
     if ($input.type === 'checkbox' || $input.type === 'radio') {
-      $input.addEventListener('click', debounce(() => {
+      $input.addEventListener('click', () => {
         autoSaveProgress($input)
-      }, 1000))
+      })
     } else {
       $input.addEventListener('blur', () => {
         autoSaveProgress($input)
