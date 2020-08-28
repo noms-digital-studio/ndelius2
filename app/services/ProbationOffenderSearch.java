02d70e8fbda4c3a087c738a4ea19cfb002acea96
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-08-28 12:31:12 +0100

    DT-965 health check for search api

--- app/services/ProbationOffenderSearch.java
+++ app/services/ProbationOffenderSearch.java
@@ -1,20 +1,43 @@
 package services;
 
 import com.google.common.collect.ImmutableMap;
+import com.typesafe.config.Config;
 import data.CourtDefendant;
 import data.MatchedOffenders;
 import interfaces.HealthCheckResult;
 import interfaces.OffenderSearch;
+import lombok.val;
+import play.Logger;
+import play.cache.AsyncCacheApi;
+import play.libs.ws.WSClient;
 import services.helpers.SearchQueryBuilder;
 
+import javax.inject.Inject;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import static interfaces.HealthCheckResult.healthy;
+import static interfaces.HealthCheckResult.unhealthy;
 import static play.libs.Json.parse;
+import static play.mvc.Http.Status.OK;
 
 public class ProbationOffenderSearch implements OffenderSearch {
+    private final WSClient wsClient;
+    private final AsyncCacheApi cache;
+    private final int cacheTime;
+    private final String apiBaseUrl;
+
+
+    @Inject
+    public ProbationOffenderSearch(Config configuration, WSClient wsClient, AsyncCacheApi cache) {
+        apiBaseUrl = configuration.getString("probation.offender.search.url");
+        this.wsClient = wsClient;
+        cacheTime = configuration.getInt("offender.api.probationAreas.cache.time.seconds");
+        this.cache = cache;
+    }
+
     @Override
     public CompletionStage<Map<String, Object>> search(String bearerToken, List<String> probationAreasFilter, String searchTerm, int pageSize, int pageNumber, SearchQueryBuilder.QUERY_TYPE queryType) {
         return CompletableFuture.completedFuture(ImmutableMap.of(
@@ -26,7 +49,20 @@ public class ProbationOffenderSearch implements OffenderSearch {
 
     @Override
     public CompletionStage<HealthCheckResult> isHealthy() {
-        return CompletableFuture.completedFuture(HealthCheckResult.healthy());
+        return wsClient.url(apiBaseUrl + "health/ping").
+                get().
+                thenApply(wsResponse -> {
+                    val healthy = wsResponse.getStatus() == OK;
+                    if (!healthy) {
+                        Logger.warn("probation offender search response status: " + wsResponse.getStatus());
+                        return unhealthy(String.format("Status %d", wsResponse.getStatus()));
+                    }
+                    return healthy(wsResponse.asJson());
+                }).
+                exceptionally(throwable -> {
+                    Logger.error("Error while checking probation offender search connectivity", throwable);
+                    return unhealthy(throwable.getLocalizedMessage());
+                });
     }
 
     @Override
---
+++ test/services/ProbationOffenderSearch_isHealthy_Test.java
@@ -0,0 +1,68 @@
+package services;
+
+import com.typesafe.config.ConfigFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import play.cache.AsyncCacheApi;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.libs.ws.WSResponse;
+
+import java.util.concurrent.CompletableFuture;
+
+import static java.util.concurrent.CompletableFuture.supplyAsync;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProbationOffenderSearch_isHealthy_Test {
+
+    private ProbationOffenderSearch probationOffenderSearch;
+
+    @Mock
+    private WSClient wsClient;
+
+    @Mock
+    private WSRequest wsRequest;
+
+    @Mock
+    private WSResponse wsResponse;
+
+    @Mock
+    private AsyncCacheApi cache;
+
+
+    @Before
+    public void setup() {
+        probationOffenderSearch = new ProbationOffenderSearch(ConfigFactory.load(), wsClient, cache);
+        when(wsClient.url(any())).thenReturn(wsRequest);
+        when(wsRequest.addHeader(any(), any())).thenReturn(wsRequest);
+        when(wsRequest.get()).thenReturn(CompletableFuture.completedFuture(wsResponse));
+    }
+
+    @Test
+    public void returnsHealthyWhenSearchApiReturns200Response() {
+        when(wsResponse.getStatus()).thenReturn(200);
+
+        assertThat(probationOffenderSearch.isHealthy().toCompletableFuture().join().isHealthy()).isEqualTo(true);
+    }
+
+    @Test
+    public void returnsUnhealthyWhenSearchApiReturnsNon200Response() {
+        when(wsResponse.getStatus()).thenReturn(404);
+
+        assertThat(probationOffenderSearch.isHealthy().toCompletableFuture().join().isHealthy()).isEqualTo(false);
+    }
+
+    @Test
+    public void returnsUnHealthyWhenSearchApiCallThrowsException() {
+        when(wsRequest.get()).thenReturn(supplyAsync(() -> { throw new RuntimeException("Boom!"); }));
+
+        assertThat(probationOffenderSearch.isHealthy().toCompletableFuture().join().isHealthy()).isEqualTo(false);
+    }
+
+}
